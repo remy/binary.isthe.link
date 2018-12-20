550e7e1d6a15e1c79d0df0792951f586d4468774
Remy Sharp <remy@remysharp.com>
2018-12-20 08:20:51 +0000

    fix: overflow

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,15 @@
 import { useState, useEffect } from 'react';
 import { Field, Button } from '../components/Form';
 import { Input as Binary } from '../components/Binary';
-import { binaryToNumber } from '../lib/numbers';
-
-const toBinary = (n, length = 8) => {
-  if (n < 0) {
-    return Array.from({ length }, (_, i) => {
-      return ((n >> i) & 1) === 1 ? 1 : 0;
-    })
-      .reverse()
-      .join('');
-  }
-
-  return n.toString(2).padStart(length, 0);
-};
+import { binaryToNumber, numberToBinary } from '../lib/numbers';
 
 const Page = props => {
+  if (!props.fed) {
+    const query = Array.from(
+      new URLSearchParams(window.location.search).entries()
+    ).reduce((acc, [key, value]) => ({ [key]: value, ...acc }), {});
+    props = Page.getInitialProps({ query });
+  }
   const [signed, setSign] = useState(props.signed);
   const [length, setLength] = useState(props.length);
   const [rep, setRep] = useState(props.rep);
@@ -81,47 +75,15 @@ const Page = props => {
   const convertToBinary = v => {
     setDValue(v);
 
-    let int = null;
-    try {
-      let res = eval(v);
-      if (typeof res === 'string') {
-        res = res.charCodeAt(0);
-      }
-      int = res;
-    } catch (e) {
-      // do nothing
-      int = parseInt(v, 10);
-    }
+    const { value, preview, binary } = numberToBinary({
+      value: v,
+      rep,
+      length,
+      signed,
+    });
 
-    let neg = int < 0;
-    if (neg) {
-      int *= -1;
-    }
-
-    if (isNaN(int)) int = 0;
-
-    let binary = null;
-
-    if (!neg || !signed) {
-      binary = toBinary(int, length);
-    } else if (rep === 0) {
-      binary = `1${toBinary(int, length).slice(-(length - 1))}`;
-    } else {
-      const twos = rep === 2 ? 1 : 0;
-      binary = `1${toBinary(twos + (int ^ (2 ** length - 1)), length).slice(
-        -(length - 1)
-      )}`;
-    }
-
-    const cmp = binaryToNumber(binary, length, signed, rep);
-
-    if (v.trim() !== cmp.trim()) {
-      setPreview(cmp);
-    } else {
-      setPreview(null);
-    }
-
-    return setValue(binary);
+    setPreview(preview);
+    setValue(binary);
   };
 
   return (
@@ -237,6 +199,7 @@ const Page = props => {
 Page.getInitialProps = ({ query }) => {
   const { length = 8, value = '00010000', signed = true, rep = 2 } = query;
   return {
+    fed: true,
     length: parseInt(length, 10),
     value,
     signed: signed === 'true' || signed === true,
